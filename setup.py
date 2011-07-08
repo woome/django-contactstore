0b7cc4a689dffcc11fd0ffc65ce713ef4b8b0b96
nferrier <none@none>
2011-07-08 12:10:54 +0100

    new version with openinviter fixes

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def _get_files():
 # django, php
 setup(
     name = "django-contactstore",
-    version = "0.1.3",
+    version = "0.1.4",
     description = "an openinviter based contact importer",
     long_description = """Allows importing and invite sending with OpenInviter using PHP.""",
     license = "GPLv2",
